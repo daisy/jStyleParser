0b00f3b681ee4a4281f84b767ecd95ecae5be193
Bert Frees <bertfrees@gmail.com>
2015-12-11 20:49:09 +0100

    Create possibility to create custom page margin areas

--- src/main/java/cz/vutbr/web/csskit/RuleMarginImpl.java
+++ src/main/java/cz/vutbr/web/csskit/RuleMarginImpl.java
@@ -6,23 +6,48 @@ import cz.vutbr.web.css.RuleMargin;
 /**
  * Implementation of RuleMargin
  * 
- * @author Bert Frees, 2012
+ * @author Bert Frees, 2012-2015
  */
 public class RuleMarginImpl extends AbstractRuleBlock<Declaration> implements RuleMargin {
-
-	private MarginArea marginArea;
-
+	
+	private enum MarginArea {
+		
+		TOPLEFTCORNER("top-left-corner"),
+		TOPLEFT("top-left"),
+		TOPCENTER("top-center"),
+		TOPRIGHT("top-right"),
+		TOPRIGHTCORNER("top-right-corner"),
+		BOTTOMLEFTCORNER("bottom-left-corner"),
+		BOTTOMLEFT("bottom-left"),
+		BOTTOMCENTER("bottom-center"),
+		BOTTOMRIGHT("bottom-right"),
+		BOTTOMRIGHTCORNER("bottom-right-corner"),
+		LEFTTOP("left-top"),
+		LEFTMIDDLE("left-middle"),
+		LEFTBOTTOM("left-bottom"),
+		RIGHTTOP("right-top"),
+		RIGHTMIDDLE("right-middle"),
+		RIGHTBOTTOM("right-bottom");
+		
+		public final String value;
+		
+		private MarginArea(String value) {
+			this.value = value;
+		}
+	}
+	
+	private final MarginArea marginArea;
+	
 	protected RuleMarginImpl(String area) {
 		for (MarginArea a : MarginArea.values()) {
 			if (a.value.equals(area)) {
 				marginArea = a;
-				break; }}
-		if (marginArea == null)
-			throw new IllegalArgumentException("Illegal value for margin area: " + area);
+				return; }}
+		throw new IllegalArgumentException("Illegal value for margin area: " + area);
 	}
-
-	public MarginArea getMarginArea() {
-		return marginArea;
+	
+	public String getMarginArea() {
+		return marginArea.value;
 	}
 
 	@Override
@@ -35,7 +60,7 @@ public class RuleMarginImpl extends AbstractRuleBlock<Declaration> implements Ru
 		StringBuilder sb = new StringBuilder();
 		
 		sb = OutputUtil.appendTimes(sb, OutputUtil.DEPTH_DELIM, depth);
-		sb.append(OutputUtil.MARGIN_AREA_OPENING).append(marginArea.value);
+		sb.append(OutputUtil.MARGIN_AREA_OPENING).append(getMarginArea());
 		
 		sb.append(OutputUtil.RULE_OPENING);
 		sb = OutputUtil.appendList(sb, list, OutputUtil.RULE_DELIM, depth + 1);
@@ -51,7 +76,7 @@ public class RuleMarginImpl extends AbstractRuleBlock<Declaration> implements Ru
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((marginArea == null) ? 0 : marginArea.hashCode());
+		result = prime * result + getMarginArea().hashCode();
 		return result;
 	}
 
@@ -67,10 +92,7 @@ public class RuleMarginImpl extends AbstractRuleBlock<Declaration> implements Ru
 		if (!(obj instanceof RuleMarginImpl))
 			return false;
 		RuleMarginImpl other = (RuleMarginImpl) obj;
-		if (marginArea == null) {
-			if (other.marginArea != null)
-				return false;
-		} else if (!marginArea.equals(other.marginArea))
+		if (!getMarginArea().equals(other.getMarginArea()))
 			return false;
 		return true;
 	}
